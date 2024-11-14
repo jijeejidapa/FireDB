d022e60df15c1c4e8630374e559e79a5a677494e
Jidapa Pakdeechote <jijeejidapa@Jidapas-MacBook-Air-2.local>
2024-11-15 02:25:30 +0700

    first commit

--- FireDB/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/FireDB.AssemblyInfo.cs
+++ FireDB/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/FireDB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FireDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+837814f9e59bb9d4fa42e2154602302e8c842830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1a470c1a2f1a5adc179c8aad627fbfa2f4fd4845")]
 [assembly: System.Reflection.AssemblyProductAttribute("FireDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FireDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
